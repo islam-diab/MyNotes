5736640e1cd6a77a3efadd29e7cb4d58da28f918
Islam Diab <islamdiab304@gmail.com>
2023-02-23 14:58:28 +0200

    create notes item

--- lib/view/widget/notes_item.dart
+++ lib/view/widget/notes_item.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffFFCC80)),
-      child: const ListTile(
-        title: Text('My frist note'),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Container(
+        padding: const EdgeInsets.only(top: 20, left: 16, bottom: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color(0xffFFCC80)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'My first note',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'This is my first note on My Notes',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 26,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                'feb32, 2023',
+                style: TextStyle(
+                  // fontSize: 28,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
