f5fb2e49e231379bb5004faa2517da79da3a1348
Islam Diab <islamdiab304@gmail.com>
2023-02-28 19:57:08 +0200

    move bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:my_notes/constant.dart';
-import 'package:my_notes/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:my_notes/models/note_model.dart';
 import 'package:my_notes/simple_bloc_observer.dart';
 import 'package:my_notes/view/notes_view.dart';
@@ -13,9 +12,9 @@ void main() async {
   await Hive.initFlutter();
 
   Bloc.observer = SimpleBlocObserver();
+  Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNotesBox);
 
-  Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyNotes());
 }
 
@@ -27,17 +26,12 @@ class MyNotes extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AddNoteCubit()),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          fontFamily: 'Poppins',
-        ),
-        home: const NotesView(),
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
       ),
+      home: const NotesView(),
     );
   }
 }
